c83a3efdb30a9438f4270d299068c13342fe0910
satishkumar1221 <satishkumar1221@gmail.com>
2020-04-22 14:49:18 -0400

    First commit

---
+++ competetive_programming_practice/Class.h
@@ -0,0 +1,19 @@
+#pragma once
+class Node
+{
+	public :
+		void *data; 
+		Node* next_data;
+		void add_elements_to_the_node();
+};
+
+extern Node* head_pointer     
+void Node :: add_elements_to_the_node()
+{
+	node *linked
+
+	if (head_pointer == NULL)
+	{
+		head = node;
+	}
+}
